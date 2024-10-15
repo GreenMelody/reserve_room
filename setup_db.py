91611a6fc6a498df958262d35abbb2b461d4bba6
minsu kim <cafri2580@gmail.com>
2024-10-15 19:08:00 +0900

    add room func init
    Signed-off-by: minsu kim <cafri2580@gmail.com>

--- setup_db.py
+++ setup_db.py
@@ -45,9 +45,13 @@ def create_tables():
     # 관리자 계정 추가
     add_admin_user(cursor)
 
+    # 기본 회의실(room1) 추가
+    add_default_room(cursor)
+
     conn.commit()
     conn.close()
 
+# 관리자 계정 추가 함수
 def add_admin_user(cursor):
     username = 'root'
     password = generate_password_hash('root')  # 비밀번호 암호화
@@ -67,6 +71,20 @@ def add_admin_user(cursor):
         print("관리자 계정이 생성되었습니다.")
     else:
         print("관리자 계정이 이미 존재합니다.")
+
+# 기본 회의실 추가 함수
+def add_default_room(cursor):
+    room_name = 'room1'
+
+    # 이미 room1 회의실이 존재하는지 확인
+    cursor.execute('SELECT * FROM rooms WHERE room_name = ?', (room_name,))
+    room = cursor.fetchone()
+
+    if not room:
+        cursor.execute('INSERT INTO rooms (room_name) VALUES (?)', (room_name,))
+        print("room1 회의실이 생성되었습니다.")
+    else:
+        print("room1 회의실이 이미 존재합니다.")
 
 if __name__ == "__main__":
     create_tables()
